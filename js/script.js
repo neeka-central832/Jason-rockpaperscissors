4b79c0939b4a75f48babe96484bc87f91072bee4
Glitch (valuable-erratic-gold) <none>
2021-11-17 23:05:00 +0000

    🚍👑 Checkpoint ./js/script.js:37463187/88 ./README.md:37463187/6 ./index.html:37463187/4

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ let randomNumber = 0;
 $(".play").click(function() {
   let input = $("input").val();
   randomNumber = Math.random();
- 
+  $(".computerChoice").text(randomNumber); 
   $(".userChoice").text(input);
 });
 
