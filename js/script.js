93e870d3d5094eee1dcdb064c15e9ba1a7175572
Glitch (rockpaperscissor-jasonzhagui) <none>
2021-12-01 22:57:52 +0000

    ✨👢 Checkpoint ./js/script.js:37463068/243:37463187/1380

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
 let randomNumber = 0;
 $(".play").click(function() {
   let input = $("input").val();
-  randomNumber = Math.random();
-  $(".computerChoice").text(randomNumber); 
+  randomNumber = Math.ceil(Math.random() * 3);
+  $(".computerChoice").text(randomNumber);
   $(".userChoice").text(input);
+  if (randomNumber === 1) {
+    $(".computerChoice").text("Rock")
+  }
+   if (randomNumber === 2) {
+    $(".computerChoice").text("Scissors")
+  }
+   if (randomNumber === 3) {
+    $(".computerChoice").text("Paper")
+  }
 });
 
 
-if randomNumber(){
-  
-  ]}
