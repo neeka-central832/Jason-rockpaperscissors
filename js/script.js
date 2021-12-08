6a8c85f5d03a0e6037e0e57d8503ff84a4ffdc6a
Glitch (rockpaperscissor-jasonzhagui) <none>
2021-12-08 23:00:50 +0000

    🌨🕴 Checkpoint ./js/script.js:37463068/3268:37463187/145:37958166/328 ./README.md:37958166/8

--- js/script.js
+++ js/script.js
@@ -5,50 +5,56 @@ $(".play").click(function() {
   randomNumber = Math.ceil(Math.random() * 3);
   $(".computerChoice").text(randomNumber);
   if (randomNumber === 1) {
-    $(".computerChoice").text("rock")
+    $(".computerChoice").text("rock");
     comp = "rock";
   }
-   if (randomNumber === 2) {
-    $(".computerChoice").text("scissors")
-  comp = "scissors";
-   }
-   if (randomNumber === 3) {
-    $(".computerChoice").text("paper")
-     comp = "paper";
-   }
-
-if (input==="rock"||input==="paper"||input==="scissors"){
-   $(".userChoice").text(input);
-} else {
-  alert("Incorrect Choice Exp. rock,paper or scissors")
-}
-  if(input === comp) {
-      $(".result").text("There's a tie!");
-  } else if(input === "rock" && comp === "scissors"||input === "paper"&& comp==="rock"|| input) {
-      $(".result").text("You win!");
+  if (randomNumber === 2) {
+    $(".computerChoice").text("scissors");
+    comp = "scissors";
+  }
+  if (randomNumber === 3) {
+    $(".computerChoice").text("paper");
+    comp = "paper";
   }
 
-  
-  
+  if (input === "rock" || input === "paper" || input === "scissors") {
+    $(".userChoice").text(input);
+  } else {
+    alert("Incorrect Choice Exp. rock,paper or scissors");
+  }
+  if (input === comp) {
+    $(".result").text("There's a tie!");
+  } else if (
+    (input === "rock" && comp === "scissors") ||
+    (input === "paper" && comp === "rock") ||
+    (input === "scissors" && comp === "paper")
+  ) {
+    $(".result").text("You win!");
+  } else {
+    $(".result").text("You lost ;(");
+  }
+
 });
 
 
-
+//
+// we did it
+// then wouldnt we add else if print(you lose)
 //is that all for day 3?
 // you can do day 3 goal 2 or start day 4!
-  // let me know if you get stuck!
-  // trying it out day 3 goal 2 on popcode first 
-  // okay great! why btw? glitch should be better, happy to help if you need
-  
-  // hint: you can use !== to check if something is *not* equal
-  // nice or statement!!!!
-  // alert state for if correct input it will put exp. rock vs paper else incorrect userinput both 
-     // so how would i do the first choice
-     // yes 
-  // its not printing now 
+// let me know if you get stuck!
+// trying it out day 3 goal 2 on popcode first
+// okay great! why btw? glitch should be better, happy to help if you need
+
+// hint: you can use !== to check if something is *not* equal
+// nice or statement!!!!
+// alert state for if correct input it will put exp. rock vs paper else incorrect userinput both
+// so how would i do the first choice
+// yes
+// its not printing now
 // thank you
 // ofc :)
 // i got the alert Incorrect Choice Exp. rock,paper and scissors
 
 // yay!!!!! awesome work today :D
-// i tied a ai 
+// i tied a ai
